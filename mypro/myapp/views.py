edcc92532d7bca5d5a8ef567b49278c9aa06362a
himaThanki <hima1006thanki@gmail.com>
2024-05-08 17:54:37 +0400

    Dynamic URL Routing In Django.

--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -27,6 +27,7 @@ def login(request):
     else:
         return render(request, 'login.html')
 
+
 def logout(request):
     auth.logout(request)
     return redirect('/')
@@ -48,10 +49,19 @@ def register(request):
                 return redirect('register')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
-                user.save();
+                user.save()
                 return redirect('login')
         else:
             messages.info(request, 'Password is not same')
             return redirect('register')
     else:
         return render(request, 'register.html')
+
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
+
+
+def counter(request):
+    posts = [1, 2, 3, 4, 5, 'tim', 'tom', 'john']
+    return render(request, 'counter.html', {'posts': posts})
